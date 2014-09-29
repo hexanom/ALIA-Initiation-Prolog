2889a0f6d4185dc0e52c1658f1f88f6377257ca5
Robin Ricard <ricard.robin@gmail.com>
2014-09-29 10:54:51 +0200

    inversion

--- lists.pl
+++ lists.pl
@@ -6,3 +6,8 @@ extraire(List,[Elt|Fin]) :- element(Elt,List,Rest),extraire(Rest,Fin).
 
 concatenation([],L2,L2).
 concatenation([E|L1],L2,[E|L3]) :- concatenation(L1,L2,L3).
+
+inv([E],[E]).
+inv([E1,E2],[E2,E1]).
+inv([E|L1],L2) :- inv(L1,LI),concatenation(LI,[E],L2).
+
