d67de5cd1050f144d6cfa9b965870f51f9c5af6b
Robin Ricard <ricard.robin@gmail.com>
2014-09-29 10:48:50 +0200

    Concat

--- lists.pl
+++ lists.pl
@@ -3,3 +3,6 @@ element(Elt,[Deb|Fin],Rest) :- element(Elt,Fin,RecurRest),Rest=[Deb|RecurRest].
 
 extraire(List,[Elt]) :- element(Elt, List, _).
 extraire(List,[Elt|Fin]) :- element(Elt,List,Rest),extraire(Rest,Fin).
+
+concatenation([],L2,L2).
+concatenation([E|L1],L2,[E|L3]) :- concatenation(L1,L2,L3).
